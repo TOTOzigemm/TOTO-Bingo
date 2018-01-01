6a4523aab8114ddcfae2847400f7ecf4e34c9696
TOTOzigemm <bowdishaw@yahoo.com>
2018-01-01 01:18:28 -0500

    New Year Goals
    happy new year!

---
+++ script/test-bingo.js
@@ -0,0 +1,84 @@
+var bingoList = [];
+// Summer Forest 1
+bingoList[1] = [
+  { name: "Win a God Card in FM2", types: [] }
+];
+bingoList[2] = [
+  { name: "Finish making a mod of FM", types: [] }
+];
+bingoList[3] = [
+  { name: "Finish the Vanilla 100% run without manips", types: [] }
+];
+bingoList[4] = [
+  { name: "Finish routing Heishin with manips", types: [] }
+];
+bingoList[5] = [
+  { name: "Do a good amount of derusting for the Ratchet hundos", types: [] }
+];
+//
+bingoList[6] = [
+  { name: "Join the 1545 again", types: [] }
+];
+bingoList[7] = [
+  { name: "Win a second God Card in FM2", types: [] }
+];
+bingoList[8] = [
+  { name: "100% the mod I make", types: [] }
+];
+bingoList[9] = [
+  { name: "Practice for Sprashfecta", types: [] }
+];
+bingoList[10] = [
+  { name: "Finish routing up to Peggy TECs with manips", types: [] }
+];
+// 
+bingoList[11] = [
+  { name: "Place top 5 in Sprashfecta", types: [] }
+];
+bingoList[12] = [
+  { name: "Finish a Ratchet and Clank hundo Trifecta", types: [] }
+];
+bingoList[13] = [
+  { name: "Win all 3 God Cards in FM2", types: [] }
+];
+// 
+bingoList[14] = [
+  { name: "Finish all the 602 games casually", types: [] }
+];
+bingoList[15] = [
+  { name: "Finish FF9 casually", types: [] }
+];
+// 
+bingoList[16] = [
+  { name: "Learn all the 602 games", types: [] }
+];
+bingoList[17] = [
+  { name: "Join and finish a FF9 'Insanity%' race", types: [] }
+];
+bingoList[18] = [
+  { name: "Finish routing Vanilla 100% with manips", types: [] }
+];
+bingoList[19] = [
+  { name: "722/722 library in FM2", types: [] }
+];
+bingoList[20] = [
+  { name: "Do another run of Digimon World 3 100%", types: [] }
+];
+// 
+bingoList[21] = [
+  { name: "Try to stream Rabi Ribi (or get all achievements if I can't)", types: [] }
+];
+bingoList[22] = [
+  { name: "Sub 24 in Vanilla 100% with manips", types: [] }
+];
+bingoList[23] = [
+  { name: "Get a really good Ratchet Trifecta time (sub 20?)", types: [] }
+];
+bingoList[24] = [
+  { name: "Sub 75 Digimon World 3 100%", types: [] }
+];
+bingoList[25] = [
+  { name: "Have a playset of every possible card in FM2", types: [] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
